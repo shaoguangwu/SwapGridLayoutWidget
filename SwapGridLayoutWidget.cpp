f867fae9396a55b1a5a08e56f584fd68b013c7aa
Wu Shaoguang <48747588+shaoguangwu@users.noreply.github.com>
2021-01-10 12:27:08 +0800

    Update SwapGridLayoutWidget.cpp

--- SwapGridLayoutWidget.cpp
+++ SwapGridLayoutWidget.cpp
@@ -1,6 +1,6 @@
 /****************************************************************************
 **
-** Copyright (C) 2021 WuShaoguang.
+** Copyright (C) 2021 Wu Shaoguang.
 **
 ** Licensed under the Apache License, Version 2.0 (the "License");
 ** you may not use this file except in compliance with the License.
